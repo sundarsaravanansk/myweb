4b3096014e2ffae1d126dca8598a66c10a56eb6d
S.Sundar <114160338+sundarsaravanansk@users.noreply.github.com>
2024-04-27 13:35:13 +0530

    Update Contact.jsx

--- src/app/Component/Contact/Contact.jsx
+++ src/app/Component/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import Contactcontent from './Contactcontent';
 
 const Contact = () => {
     return (
-        <div className='relative w-[100%] md:h-[100vh]' id='contact'>
+        <div className='relative w-[100%] md:h-[100vh]'>
        
        
         <Contactcontent/>
