21bb7ba66cafe16511cfa30e379ed191b021d657
S.Sundar <114160338+sundarsaravanansk@users.noreply.github.com>
2024-04-27 13:37:31 +0530

    Update Skill.jsx

--- src/app/Component/Skill/Skill.jsx
+++ src/app/Component/Skill/Skill.jsx
@@ -4,7 +4,7 @@ import "./skill.css";
 
 const Skill = () => {
     return (
-        <div className='relative w-[100%] md:h-[100vh] pt-12 pb-12 md:pt-0 md:pb-0' id='Skill'>
+        <div className='relative w-[100%] md:h-[100vh] pt-12 pb-12 md:pt-10 md:pb-0' id='Skill'>
        
         
         <Skillcontent/>
